0bd4198af0c34ed874c371db8cadbed68f905f2f
Jamprey25 <53832941+Jamprey25@users.noreply.github.com>
2026-04-09 13:21:20 -0400

    added web analytics from vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -40,6 +41,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col bg-zinc-950 text-zinc-50">
         {children}
+        <Analytics />
       </body>
     </html>
   );
